c4f1d708909289d58dc08cbd11c1236aaf667f6e
Vladimir Shidlovsky <vl.shidlovsky@gmail.com>
2012-10-09 10:01:25 +0300

    Resolve issue for show action crumb.

--- lib/gretel/resource_crumbs.rb
+++ lib/gretel/resource_crumbs.rb
@@ -9,14 +9,16 @@ module Gretel
 
     def index_crumb(name)
       crumb(name) do
-        link(model_plural_name(name), send("#{name}_path"))
+        url = send("#{name}_path") rescue "#"
+        link(model_plural_name(name), url)
       end
     end
 
     def new_crumb(name)
       new_name = "new_#{name.to_s.singularize}".to_sym
       crumb(new_name) do
-        link(resource_name(name), send("#{new_name}_path"))
+        url = send("#{new_name}_path") rescue "#"
+        link(resource_name(name), url)
         parent name
       end
     end
@@ -24,7 +26,8 @@ module Gretel
     def edit_crumb(name)
       edit_name = "edit_#{name.to_s.singularize}".to_sym
       crumb(edit_name) do |resource|
-        link(resource_title(resource), send("#{edit_name}_path", resource))
+        url = send(["#{edit_name}_path", "#{name}_path"].find {|m| respond_to?(m)}) rescue "#"
+        link(resource_title(resource), url, resource)
         parent name
       end 
     end
